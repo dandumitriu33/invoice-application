8174470d5b28786c62828ba5f42d9484bca3019b
Dan Dumitriu <dandumitriu33@gmail.com>
2021-02-04 15:01:31 +0200

    Add Error logging via ILogger

--- InvoiceApplication/InvoiceApplication.WEB/Controllers/ErrorController.cs
+++ InvoiceApplication/InvoiceApplication.WEB/Controllers/ErrorController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,13 @@ namespace InvoiceApplication.WEB.Controllers
 {
     public class ErrorController : Controller
     {
+        private readonly ILogger<ErrorController> _logger;
+
+        public ErrorController(ILogger<ErrorController> logger)
+        {
+            _logger = logger;
+        }
+
         [Route("Error/{statusCode}")]
         public IActionResult HttpStatusCodeHandler(int statusCode)
         {
@@ -16,9 +24,11 @@ namespace InvoiceApplication.WEB.Controllers
             {
                 case 404:
                     ViewBag.ErrorMessage = "404 - Page not found.";
+                    _logger.LogWarning($"404 error.");
                     break;
                 default:
                     ViewBag.ErrorMessage = statusCode.ToString() + " - That's all we know.";
+                    _logger.LogWarning($"{statusCode} error.");
                     break;
             }
             return View("Error");
@@ -29,6 +39,7 @@ namespace InvoiceApplication.WEB.Controllers
         {
             var exceptionDetails = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
             ViewBag.ErrorMessage = exceptionDetails.Error.Message;
+            _logger.LogError($"The path {exceptionDetails.Path} threw an exception {exceptionDetails.Error}");
             return View("Error");
         }
     }
